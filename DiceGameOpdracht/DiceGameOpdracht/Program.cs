aefe6484633307f6086d627df2adae3953a54cb1
jip erkelens <59746858+multityy@users.noreply.github.com>
2021-02-24 12:14:15 +0100

    Update Program.cs

--- DiceGameOpdracht/DiceGameOpdracht/Program.cs
+++ DiceGameOpdracht/DiceGameOpdracht/Program.cs
@@ -6,7 +6,29 @@ namespace DiceGameOpdracht
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("[1] read the rules");
+            Console.WriteLine("[2] Start Game");
+            Console.WriteLine("[3] Exit Game");
+
+            string ans = Console.ReadLine();
+            if (ans != null && ans == "1")
+            {
+                Console.WriteLine("rules: ");
+               
+            }
+            else if (ans != null && ans == "2")
+            {
+                Console.WriteLine("start game");
+                
+            }
+            else if (ans != null && ans == "3")
+            {
+                Console.Write("quit game");
+            }
+            else
+            {
+                Console.Write("ERROR");
+            }
         }
     }
 }
