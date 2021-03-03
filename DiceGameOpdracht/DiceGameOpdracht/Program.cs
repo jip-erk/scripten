bcff4d16b95539879b28e45fb151e95b733616d1
jip erkelens <59746858+multityy@users.noreply.github.com>
2021-03-03 13:25:18 +0100

    yes

--- DiceGameOpdracht/DiceGameOpdracht/Program.cs
+++ DiceGameOpdracht/DiceGameOpdracht/Program.cs
@@ -6,6 +6,10 @@ namespace DiceGameOpdracht
     {
         static void Main(string[] args)
         {
+
+
+            Random rand = new Random();
+
             Console.WriteLine("[1] read the rules");
             Console.WriteLine("[2] Start Game");
             Console.WriteLine("[3] Exit Game");
@@ -19,6 +23,8 @@ namespace DiceGameOpdracht
             else if (ans != null && ans == "2")
             {
                 Console.WriteLine("start game");
+
+                startGame();
                 
             }
             else if (ans != null && ans == "3")
@@ -30,7 +36,46 @@ namespace DiceGameOpdracht
                 Console.Write("ERROR");
             }
 
+            void startGame()
+            {
+
+                int Players;
+                do
+                {
+                    Console.WriteLine("enter how many players want to play (max 4) ");
+                    String stringallplayers = Console.ReadLine();
+                    Players = int.Parse(stringallplayers);
+                   
+
+                   
+                }
+                while (Players > 4 );
+
+                int[] players1 = new int[Players] ;
+
+                
+
+               for (int i = 0; i < Players; i++)
+               {
+                    int[] playerrolled = new int[i];
+
+                    playerrolled[0] = [1];
+
+                    int dicenumb = rand.Next(1, 12);
+                    int dicenumb2 = rand.Next(1, 12);
+                    int totalRoll = dicenumb + dicenumb2;
+                    Console.WriteLine($"player" + "dice 1:" + dicenumb + "   dice 2:" + dicenumb2);
+
+                }
+
+               
+
+              
+            }
+
             
         }
+
+        
     }
 }
