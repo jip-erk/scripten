e1e127f567e1d311c39dd1b3fb2cdb00543bc4ed
jipErkelens <79573058+jipErkelens@users.noreply.github.com>
2021-03-10 11:08:54 +0100

    assignment_week1

---
+++ week_1_Assignment_jip_erkelens/week_1_Assignment_jip_erkelens/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week_1_Assignment_jip_erkelens
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 7);
+
+            #region list
+
+            var items1 = new List<string>() { "item1", "item2", "item3", "item4", "item5" };
+
+            var items2 = new List<string>() { "item1", "item2", "item3" };
+
+            items1.AddRange(items2);
+
+            items1.ForEach(Console.WriteLine);
+
+            #endregion
+
+            
+
+            Console.WriteLine("enter item: ");
+            listchecker();
+
+             void listchecker()
+            {
+
+                if (items1.Contains(Console.ReadLine()))
+                {
+                    Console.WriteLine("This item is in the list!");
+                }
+                else
+                {
+                    
+                    Console.WriteLine("this item is not in the list");
+                    listchecker();
+                }
+
+                items1.RemoveAt(random);
+                items1.Sort();
+                items1.ForEach(Console.WriteLine);
+
+                items1.RemoveRange(4, 8 );
+                items1.Reverse();
+                items1.ForEach(Console.WriteLine);
+
+            }
+
+            
+
+
+
+
+        }
+    }
+}
